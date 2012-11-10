79ee8174781a575cb25e628855ea7f2da6436f58
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2012-11-10 19:19:27 +0100

    Windows

---
+++ MCT/Path.h
@@ -0,0 +1,24 @@
+#ifndef PATH_H
+#define PATH_H
+
+#ifdef WIN32
+
+#define TOOLBOX "media/toolbox.png"
+#define POINTER "media/pointer.png"
+#define SPLASH "media/splash.png"
+#define ALERTBOX "media/alertbox.png"
+#define ROAD_PATH "media/road.png"
+#define INDUSTRIA_PATH "media/industria.png"
+#define RESIDENCIA_MODERNA_PATH "media/residencia_moderna.png"
+#define AMARILLA_PATH "media/amarilla.png"
+#define OFICINA_PATH "media/oficina.png"
+#define CLEAN_PATH "media/template.png"
+#define VALLA_PATH "media/valla.png"
+
+
+
+#endif
+
+
+
+#endif
